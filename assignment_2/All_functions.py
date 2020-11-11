d6674da32443d1955bf1271be9b5e7d1e257ba3d
adithyan-kv <adithyankv007@gmail.com>
2020-11-12 01:33:35 +0530

    Implemented linear contrast stretch
     -A linear contrast stretch with a gain parameter was written

--- assignment_2/All_functions.py
+++ assignment_2/All_functions.py
@@ -1,5 +1,25 @@
+import skimage.io as io
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def main():
-    print("something")
+    linear_contrast_strech('LowLight_1.png')
+
+
+def linear_contrast_strech(image_path):
+    image_data = io.imread(image_path)
+    A = np.max(image_data)
+    B = np.min(image_data)
+    k = 255
+    if (A - B) != 0:
+        gain = k / (A - B)
+    # if image is one uniform intensity, no contrast enhancement possible
+    else:
+        gain = 1
+    stretched_image = image_data * gain
+    plt.imshow(stretched_image, cmap='gray')
+    plt.show()
 
 
 if __name__ == "__main__":
