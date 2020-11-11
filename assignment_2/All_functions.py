740f8c0c673d02ce2d63d9b8d01ba6ca573e2db9
adithyan-kv <adithyankv007@gmail.com>
2020-11-12 02:08:52 +0530

    Added function for visualizing images
     -Added a function which shows the original and enhanced images side by
      side

--- assignment_2/All_functions.py
+++ assignment_2/All_functions.py
@@ -4,10 +4,24 @@ import numpy as np
 
 
 def main():
-    linear_contrast_strech('LowLight_1.png')
+    original_img_1 = io.imread('LowLight_1.png')
+    original_img_2 = io.imread('LowLight_2.png')
+    linear_stretched_img_1 = linear_contrast_stretch('LowLight_1.png')
+    linear_stretched_img_2 = linear_contrast_stretch('LowLight_2.png')
+    plot_side_by_side(original_img_1, linear_stretched_img_1)
+    plot_side_by_side(original_img_2, linear_stretched_img_2)
 
 
-def linear_contrast_strech(image_path):
+def plot_side_by_side(image_1, image_2):
+    _, plots = plt.subplots(1, 2)
+    plots[0].imshow(image_1, cmap='gray', vmax=255, vmin=0)
+    plots[0].set_title('Original image')
+    plots[1].imshow(image_2, cmap='gray', vmax=255, vmin=0)
+    plots[1].set_title('Contrast Enhanced')
+    plt.show()
+
+
+def linear_contrast_stretch(image_path):
     image_data = io.imread(image_path)
     A = np.max(image_data)
     B = np.min(image_data)
@@ -18,8 +32,7 @@ def linear_contrast_strech(image_path):
     else:
         gain = 1
     stretched_image = image_data * gain
-    plt.imshow(stretched_image, cmap='gray')
-    plt.show()
+    return stretched_image
 
 
 if __name__ == "__main__":
