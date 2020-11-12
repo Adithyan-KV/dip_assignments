3f776fe42f14f7080afefb5d315ebd6bc123858d
adithyan-kv <adithyankv007@gmail.com>
2020-11-13 01:24:38 +0530

    Added power parameter to power law stretch
     -Previously all stretches were using the same gamma
     -This only worked for low light images and not hazy images. So now
      now using a gamma parameter.

--- assignment_2/All_functions.py
+++ assignment_2/All_functions.py
@@ -6,17 +6,24 @@ import numpy as np
 def main():
     original_img_1 = io.imread('LowLight_1.png')
     original_img_2 = io.imread('LowLight_2.png')
+    original_img_3 = io.imread('Hazy.png')
     linear_stretched_img_1 = linear_contrast_stretch('LowLight_1.png')
     linear_stretched_img_2 = linear_contrast_stretch('LowLight_2.png')
-    plot_side_by_side(original_img_1, linear_stretched_img_1)
-    plot_side_by_side(original_img_2, linear_stretched_img_2)
+    plot_side_by_side(original_img_1, linear_stretched_img_1, 'Linear stretch')
+    plot_side_by_side(original_img_2, linear_stretched_img_2, 'Linear stretch')
 
-    power_stretched_image = power_law_contrast_stretch('LowLight_2.png')
-    plot_side_by_side(original_img_2, power_stretched_image)
+    power_stretched_image_1 = power_law_contrast_stretch(
+        'LowLight_2.png', 1 / 2)
+    power_stretched_image_2 = power_law_contrast_stretch('Hazy.png', 2)
+    plot_side_by_side(original_img_2, power_stretched_image_1,
+                      'Power law stretch')
+    plot_side_by_side(original_img_3, power_stretched_image_2,
+                      'Power law stretch')
 
 
-def plot_side_by_side(image_1, image_2):
-    _, plots = plt.subplots(1, 2)
+def plot_side_by_side(image_1, image_2, title):
+    fig, plots = plt.subplots(1, 2)
+    fig.suptitle(title)
     plots[0].imshow(image_1, cmap='gray', vmax=255, vmin=0)
     plots[0].set_title('Original image')
     plots[1].imshow(image_2, cmap='gray', vmax=255, vmin=0)
@@ -33,10 +40,11 @@ def linear_contrast_stretch(image_path):
     return stretched_image
 
 
-def power_law_contrast_stretch(image_path):
-    image_data = io.imread(image_path) / 255
-    enhanced_image = image_data**(1 / 2)
-    return enhanced_image * 255
+def power_law_contrast_stretch(image_path, power):
+    image_data = io.imread(image_path)
+    normalized_image_data = image_data / 255
+    enhanced_image = normalized_image_data**(power) * 255
+    return enhanced_image
 
 
 if __name__ == "__main__":
