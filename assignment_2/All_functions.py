7e4c7a031e12c28f3a8df9907a27b221b932ee0b
adithyan-kv <adithyankv007@gmail.com>
2020-11-14 01:12:04 +0530

    Implemented Adaptive Histogram Equalization(AHE)
     -Histogram equalization after splitting image into small patches
     -Not vectorized so very slow
     -Have to implement contrast limiting for CLAHE

--- assignment_2/All_functions.py
+++ assignment_2/All_functions.py
@@ -1,3 +1,4 @@
+from skimage.util.shape import view_as_blocks
 import skimage.io as io
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,24 +12,27 @@ def main():
     hazy = io.imread('Hazy.png')
     linear_stretched_img_1 = linear_contrast_stretch('LowLight_1.png')
     linear_stretched_img_2 = linear_contrast_stretch('LowLight_2.png')
-    plot_side_by_side(low_light_1, linear_stretched_img_1, 'Linear stretch')
-    plot_side_by_side(low_light_2, linear_stretched_img_2, 'Linear stretch')
+    # plot_side_by_side(low_light_1, linear_stretched_img_1, 'Linear stretch')
+    # plot_side_by_side(low_light_2, linear_stretched_img_2, 'Linear stretch')
 
     power_stretched_image_1 = power_law_contrast_stretch(
         'LowLight_2.png', 1 / 2)
     power_stretched_image_2 = power_law_contrast_stretch('Hazy.png', 2)
-    plot_side_by_side(low_light_2, power_stretched_image_1,
-                      'Power law stretch')
-    plot_side_by_side(hazy, power_stretched_image_2,
-                      'Power law stretch')
+    # plot_side_by_side(low_light_2, power_stretched_image_1,
+    #   'Power law stretch')
+    # plot_side_by_side(hazy, power_stretched_image_2,
+    #   'Power law stretch')
     histogram_eq_image_1 = histogram_equalize('LowLight_2.png')
-    plot_side_by_side(low_light_2, histogram_eq_image_1, 'hist eq')
+    # plot_side_by_side(low_light_2, histogram_eq_image_1, 'hist eq')
     histogram_eq_image_2 = histogram_equalize('LowLight_3.png')
     histogram_eq_image_3 = histogram_equalize('Hazy.png')
     histogram_eq_image_4 = histogram_equalize('StoneFace.png')
-    plot_side_by_side(low_light_3, histogram_eq_image_2, 'hist eq')
-    plot_side_by_side(hazy, histogram_eq_image_3, 'hist eq')
-    plot_side_by_side(stone_face, histogram_eq_image_4, 'hist eq')
+    # plot_side_by_side(low_light_3, histogram_eq_image_2, 'hist eq')
+    # plot_side_by_side(hazy, histogram_eq_image_3, 'hist eq')
+    # plot_side_by_side(stone_face, histogram_eq_image_4, 'hist eq')
+    clahe_image_1 = contrast_limited_histogram_equalize('StoneFace.png')
+    plt.imshow(clahe_image_1, cmap='gray')
+    plt.show()
 
 
 def plot_side_by_side(image_1, image_2, title):
@@ -63,6 +67,25 @@ def histogram_equalize(image_path):
     hist, _ = np.histogram(image_data, bins=255, range=(0, 255))
     cdf = np.array([sum(hist[:i + 1]) / no_total_pixels for i in range(256)])
     enhanced_image = cdf[image_data] * 255
+    return enhanced_image
+
+
+def contrast_limited_histogram_equalize(image_path):
+    size = 8
+    image_data = io.imread(image_path)
+    enhanced_image = np.zeros_like(image_data)
+    no_total_pixels = image_data.size
+    rows, columns = image_data.shape
+    for i in range(rows // size):
+        for j in range(columns // size):
+            block = image_data[i * size:(i + 1) *
+                               size, j * size:(j + 1) * size]
+            hist, _ = np.histogram(image_data, bins=255, range=(0, 255))
+            cdf = np.array(
+                [sum(hist[:i + 1]) / no_total_pixels for i in range(256)])
+            enhanced_block = cdf[block] * 255
+            enhanced_image[i * size:(i + 1) *
+                           size, j * size:(j + 1) * size] = enhanced_block
     return enhanced_image
 
 
