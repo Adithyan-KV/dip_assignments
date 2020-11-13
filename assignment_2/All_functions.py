b60f080e0ea219eebef9d2a219661edc175bea7a
adithyan-kv <adithyankv007@gmail.com>
2020-11-14 02:48:10 +0530

    Refactored CLAHE for 20x improvement
     -Previously i was writing the function for CDF from scratch, but now
      using numpy's cumsum function.
     -The same has been replaced in the normal histogram equalization case
      as well.
     -Now the program running about 20x faster

--- assignment_2/All_functions.py
+++ assignment_2/All_functions.py
@@ -1,4 +1,3 @@
-from skimage.util.shape import view_as_blocks
 import skimage.io as io
 import matplotlib.pyplot as plt
 import numpy as np
@@ -65,7 +64,8 @@ def histogram_equalize(image_path):
     image_data = io.imread(image_path)
     no_total_pixels = image_data.size
     hist, _ = np.histogram(image_data, bins=255, range=(0, 255))
-    cdf = np.array([sum(hist[:i + 1]) / no_total_pixels for i in range(256)])
+    cdf = np.ones(256)
+    cdf[:-1] = hist.cumsum() / no_total_pixels
     enhanced_image = cdf[image_data] * 255
     return enhanced_image
 
@@ -75,22 +75,20 @@ def contrast_limited_histogram_equalize(image_path):
     image_data = io.imread(image_path)
     enhanced_image = np.zeros_like(image_data)
     rows, columns = image_data.shape
-    for i in range(rows // size):
-        for j in range(columns // size):
-            block = image_data[i * size:(i + 1) *
-                               size, j * size:(j + 1) * size]
+    for i in range(0, rows, size):
+        for j in range(0, columns, size):
+            block = image_data[i:i + size, j:j + size]
+            total_pixels = block.size
             hist, _ = np.histogram(block, bins=255, range=(0, 255))
-            cl_hist = contrast_limit_histogram(hist)
-            cdf = np.array(
-                [sum(cl_hist[:i + 1]) / size**2 for i in range(256)])
+            cdf = np.ones(256)
+            cdf[:-1] = hist.cumsum() / total_pixels
             enhanced_block = cdf[block] * 255
-            enhanced_image[i * size:(i + 1) *
-                           size, j * size:(j + 1) * size] = enhanced_block
+            enhanced_image[i:i + size, j:j + size] = enhanced_block
     return enhanced_image
 
 
 def contrast_limit_histogram(histogram_freq):
-    threshold = 64
+    threshold = 40
     hist_size = len(histogram_freq)
     excess = histogram_freq[histogram_freq > threshold].sum()
     distribute = excess // hist_size
