93e6fe49400d60737a48a509fac612997201c580
adithyan-kv <adithyankv007@gmail.com>
2021-01-07 00:25:15 +0530

    Refactored the questions into their own functions

--- assignment_4/Wrapper.py
+++ assignment_4/Wrapper.py
@@ -5,25 +5,32 @@ import skimage.transform as tfm
 
 
 def main():
-    # Question 1
+    question_1()
+    question_2()
+    question_3()
+
+
+def question_1():
     pass
 
-    # # Question 2
-    # # part a
-    # noisy = io.imread('noisy-book1.png')
-    # gauss_denoised = af.gaussian_denoise(noisy, 7, 3)
-    # median_denoised = af.median_filter_denoise(noisy, 5)
-    # fig, plots = plt.subplots(1, 3)
-    # fig.suptitle('Comparing median and gaussian filtering')
-    # plots[0].imshow(noisy, cmap='gray')
-    # plots[0].set_title('Original Image')
-    # plots[1].imshow(gauss_denoised, cmap='gray')
-    # plots[1].set_title('Gaussian filtered')
-    # plots[2].imshow(median_denoised, cmap='gray')
-    # plots[2].set_title('Median filtered')
-    # plt.show()
 
-    # Question 3
+def question_2():
+    # part a
+    noisy = io.imread('noisy-book1.png')
+    gauss_denoised = af.gaussian_denoise(noisy, 7, 3)
+    median_denoised = af.median_filter_denoise(noisy, 5)
+    fig, plots = plt.subplots(1, 3)
+    fig.suptitle('Comparing median and gaussian filtering')
+    plots[0].imshow(noisy, cmap='gray')
+    plots[0].set_title('Original Image')
+    plots[1].imshow(gauss_denoised, cmap='gray')
+    plots[1].set_title('Gaussian filtered')
+    plots[2].imshow(median_denoised, cmap='gray')
+    plots[2].set_title('Median filtered')
+    plt.show()
+
+
+def question_3():
     barbara = io.imread("barbara.tif")
     downsampled = af.downsample(barbara, 2)
     decimated = af.decimate(barbara, 2)
