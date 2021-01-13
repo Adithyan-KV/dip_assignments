65385d792d6d0ac2bbcaafc503a16c87bb8ec0f1
adithyan-kv <adithyankv007@gmail.com>
2021-01-14 02:32:28 +0530

    Bug Fix
     -Was using Ix and Iy instead of their squared versions for computing
      response

--- assignment_5/AllFunctions.py
+++ assignment_5/AllFunctions.py
@@ -7,7 +7,7 @@ import scipy.ndimage as ndi
 
 
 def main():
-    window_image = io.imread('win.jpg')
+    window_image = io.imread('bar.jpg')
     window_img_grayscale = col.rgb2gray(window_image)
     corner_map = harris_corner_detect(window_img_grayscale)
 
@@ -34,8 +34,8 @@ def harris_corner_detect(image_data):
             start_x, end_x = i - offset, i + offset + 1
             start_y, end_y = j - offset, j + offset + 1
 
-            Ix_sq_win = Ix[start_x:end_x, start_y:end_y]
-            Iy_sq_win = Iy[start_x:end_x, start_y:end_y]
+            Ix_sq_win = Ix_sq[start_x:end_x, start_y:end_y]
+            Iy_sq_win = Iy_sq[start_x:end_x, start_y:end_y]
             IxIy_win = IxIy[start_x:end_x, start_y:end_y]
 
             Sx_sq = Ix_sq_win.sum()
@@ -48,7 +48,17 @@ def harris_corner_detect(image_data):
             R[i, j] = np.linalg.det(M) - k * (np.trace(M)**2)
 
     print(R.max(), R.min())
-    corner_map = np.abs(R) > 15
+    corner_map = np.abs(R) > 30
+
+    fig, plots = plt.subplots(1, 3)
+    fig.suptitle('Question 1 (c):Constrained Least Squares Filtering')
+    plots[0].imshow(image_data, cmap='gray')
+    plots[0].set_title('Original Image')
+    plots[1].imshow(R, cmap='gray')
+    plots[1].set_title('R')
+    plots[2].imshow(corner_map, cmap='gray')
+    plots[2].set_title('Corners detected')
+    plt.show()
 
     return corner_map
 
