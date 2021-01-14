085dbfc77f94630b295da28208c282386a2d4ef0
adithyan-kv <adithyankv007@gmail.com>
2021-01-14 23:09:58 +0530

    Wrote wrapper code for rotation and corner detection

--- assignment_5/Wrapper.py
+++ assignment_5/Wrapper.py
@@ -5,8 +5,8 @@ import AllFunctions as af
 
 
 def main():
-    question_1()
-    # question_2()
+    # question_1()
+    question_2()
 
 
 def question_1():
@@ -46,6 +46,35 @@ def question_1():
 
     # visualizing multiple thresholds
     af.visualize_multiple_thresholds(car_image, R_values_2)
+
+
+def question_2():
+    # rotation
+    window_image = io.imread('win.jpg')
+    car_image = io.imread('car.jpg')
+    car_img_grayscale = col.rgb2gray(car_image)
+    window_img_grayscale = col.rgb2gray(window_image)
+    image_rot_1, locations_rot_1 = af.rotate_and_corner_detect(
+        window_img_grayscale, 45)
+    image_rot_2, locations_rot_2 = af.rotate_and_corner_detect(
+        window_img_grayscale, 90)
+    _, _, locations_1 = af.harris_corner_detect(
+        window_img_grayscale, 3, 2, 0.1)
+    fig, plots = plt.subplots(1, 3)
+    fig.suptitle('Question 1 :Rotated Images')
+    plots[0].imshow(window_img_grayscale, cmap='gray')
+    plots[0].scatter(locations_1[1],
+                     locations_1[0], s=5, c='yellow')
+    plots[0].set_title('Original Image')
+    plots[1].imshow(image_rot_2, cmap='gray')
+    plots[1].set_title('Rotated Image: 90 deg')
+    plots[1].scatter(locations_rot_2[1],
+                     locations_rot_2[0], s=5, c='yellow')
+    plots[2].imshow(image_rot_1, cmap='gray')
+    plots[2].scatter(locations_rot_1[1],
+                     locations_rot_1[0], s=5, c='yellow')
+    plots[2].set_title('Rotated Image: 45 deg')
+    plt.show()
 
 
 if __name__ == "__main__":
