343d4561bf85bc919e4e0d4bc8a3d04e3059db79
adithyan-kv <adithyankv007@gmail.com>
2021-01-14 23:51:02 +0530

    Wrote wrapper code for noise addition, Q2 wrapper code complete

--- assignment_5/Wrapper.py
+++ assignment_5/Wrapper.py
@@ -5,7 +5,7 @@ import AllFunctions as af
 
 
 def main():
-    # question_1()
+    question_1()
     question_2()
 
 
@@ -49,19 +49,23 @@ def question_1():
 
 
 def question_2():
-    # rotation
+    print("Running Question 2: might take some time between each plot...")
     window_image = io.imread('win.jpg')
     car_image = io.imread('car.jpg')
     car_img_grayscale = col.rgb2gray(car_image)
     window_img_grayscale = col.rgb2gray(window_image)
+
+    # rotation
     image_rot_1, locations_rot_1 = af.rotate_and_corner_detect(
         window_img_grayscale, 45)
     image_rot_2, locations_rot_2 = af.rotate_and_corner_detect(
         window_img_grayscale, 90)
     _, _, locations_1 = af.harris_corner_detect(
         window_img_grayscale, 3, 2, 0.1)
+    _, _, locations_2 = af.harris_corner_detect(
+        car_img_grayscale, 3, 2, 0.1)
     fig, plots = plt.subplots(1, 3)
-    fig.suptitle('Question 1 :Rotated Images')
+    fig.suptitle('Question 2 :Rotated Images')
     plots[0].imshow(window_img_grayscale, cmap='gray')
     plots[0].scatter(locations_1[1],
                      locations_1[0], s=5, c='yellow')
@@ -74,6 +78,48 @@ def question_2():
     plots[2].scatter(locations_rot_1[1],
                      locations_rot_1[0], s=5, c='yellow')
     plots[2].set_title('Rotated Image: 45 deg')
+    plt.show()
+
+    # scaling
+    image_scaled_1, locations_sc_1 = af.scale_and_corner_detect(
+        car_img_grayscale, 1.2)
+    image_scaled_2, locations_sc_2 = af.scale_and_corner_detect(
+        car_img_grayscale, 0.2)
+    fig, plots = plt.subplots(1, 3)
+    fig.suptitle('Question 2 :Scaled Images')
+    plots[0].imshow(car_img_grayscale, cmap='gray')
+    plots[0].scatter(locations_2[1],
+                     locations_2[0], s=5, c='yellow')
+    plots[0].set_title('Original Image')
+    plots[1].imshow(image_scaled_1, cmap='gray')
+    plots[1].set_title('Scaled 1.2x')
+    plots[1].scatter(locations_sc_1[1],
+                     locations_sc_1[0], s=5, c='yellow')
+    plots[2].imshow(image_scaled_2, cmap='gray')
+    plots[2].scatter(locations_sc_2[1],
+                     locations_sc_2[0], s=5, c='yellow')
+    plots[2].set_title('Scaled 0.2x')
+    plt.show()
+
+    # Noise addition
+    image_noisy_1, locations_noisy_1 = af.add_noise_and_corner_detect(
+        car_img_grayscale)
+    image_noisy_2, locations_noisy_2 = af.add_noise_and_corner_detect(
+        car_img_grayscale, 's&p')
+    fig, plots = plt.subplots(1, 3)
+    fig.suptitle('Question 2 :Noisy Images')
+    plots[0].imshow(car_img_grayscale, cmap='gray')
+    plots[0].scatter(locations_2[1],
+                     locations_2[0], s=5, c='yellow')
+    plots[0].set_title('Original Image')
+    plots[1].imshow(image_noisy_1, cmap='gray')
+    plots[1].set_title('Gaussian Noise added')
+    plots[1].scatter(locations_noisy_1[1],
+                     locations_noisy_1[0], s=5, c='yellow')
+    plots[2].imshow(image_noisy_2, cmap='gray')
+    plots[2].set_title('Salt and pepper noise added')
+    plots[2].scatter(locations_noisy_2[1],
+                     locations_noisy_2[0], s=5, c='yellow')
     plt.show()
 
 
